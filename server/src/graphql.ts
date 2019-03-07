5de5e20cfe8fef01d8fc0bc9fc47647313fc773e
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2019-03-08 00:30:00 +0100

    build: switch from netlify to now

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,4 +1,4 @@
 import { ApolloServer } from 'apollo-server-lambda';
-import { serverConfig } from '../core/server';
+import { serverConfig } from './core/server';
 
 export const handler = new ApolloServer(serverConfig).createHandler();
